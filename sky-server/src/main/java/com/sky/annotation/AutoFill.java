d8d7668039a64c6fb37631dd138426628fc4bd92
Wang <2531709519@qq.com>
2024-04-12 15:53:00 +0800

    苍穹外卖公共字段填充AOP

---
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -0,0 +1,23 @@
+package com.sky.annotation;
+
+import com.sky.enumeration.OperationType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * ClassName: AutoFill
+ * Package: com.sky.annotation
+ * Description:自定义注解，用于标识需要自动填充的方法
+ *
+ * @Author x4j
+ * @Create 2024/4/12 14:48
+ * @Version 1.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoFill {
+    OperationType value();
+}
---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,88 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.scheduling.quartz.LocalDataSourceJobStore;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * ClassName: AutoFillAspect
+ * Package: com.sky.aspect
+ * Description:自定义填充，实现公共字段自动填充逻辑
+ *
+ * @Author x4j
+ * @Create 2024/4/12 15:01
+ * @Version 1.0
+ */
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    /**
+     * 前置通知，公共字段赋值
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        //获取被拦截操作类型
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);
+        OperationType operationType = autoFill.value();
+
+        //获取被拦截方法的参数-实体对象
+        Object[] args = joinPoint.getArgs();
+        if(args == null || args.length == 0){
+            return;
+        }
+        Object entity = args[0];
+
+        //准备赋值数据
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+
+        //通过反射赋值
+        if(operationType == OperationType.INSERT){
+            try{
+                Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                setCreateTime.invoke(entity, now);
+                setUpdateTime.invoke(entity, now);
+                setCreateUser.invoke(entity, currentId);
+                setUpdateUser.invoke(entity, currentId);
+            }catch (Exception ex){
+                ex.getStackTrace();
+            }
+        }else if(operationType == OperationType.UPDATE){
+            try{
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                setUpdateTime.invoke(entity, now);
+                setUpdateUser.invoke(entity, currentId);
+            }catch (Exception ex){
+                ex.getStackTrace();
+            }
+        }
+    }
+}
